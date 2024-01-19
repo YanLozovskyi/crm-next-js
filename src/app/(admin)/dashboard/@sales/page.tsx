9d9a21cff37060a3fc1c474ab3438b3d10d523e6
YanLozovskyi <yanlozovskiy@gmail.com>
2024-01-19 17:01:46 +0200

    copied full project

--- src/app/(admin)/dashboard/@sales/page.tsx
+++ src/app/(admin)/dashboard/@sales/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getSummaryPromotions } from '@/lib/api';
+import { getSummarySales } from '@/lib/api';
 import SummaryTable from '@/app/components/summary-table';
 import SummaryTableHeader from '@/app/components/summary-table-header';
 import SummaryTableCell from '@/app/components/summary-table-cell';
@@ -8,24 +8,24 @@ import DashboardCard from '@/app/components/dashboard-card';
 export interface PageProps {}
 
 export default async function Page({}: PageProps) {
-  const data = await getSummaryPromotions();
+  const data = await getSummarySales();
 
   return (
-    <DashboardCard label="Promotions">
+    <DashboardCard label="Sales details">
       <SummaryTable
         headers={
           <>
             <SummaryTableHeader>Company</SummaryTableHeader>
-            <SummaryTableHeader>Name</SummaryTableHeader>
-            <SummaryTableHeader align="center">%</SummaryTableHeader>
+            <SummaryTableHeader align="center">Sold</SummaryTableHeader>
+            <SummaryTableHeader align="center">Income</SummaryTableHeader>
           </>
         }
       >
-        {data.map(({ promotionId, promotionName, companyTitle, discount }) => (
-          <tr key={promotionId}>
+        {data.map(({ companyId, companyTitle, sold, income }) => (
+          <tr key={companyId}>
             <SummaryTableCell>{companyTitle}</SummaryTableCell>
-            <SummaryTableCell>{promotionName}</SummaryTableCell>
-            <SummaryTableCell align="center">{`-${discount}%`}</SummaryTableCell>
+            <SummaryTableCell align="center">{sold}</SummaryTableCell>
+            <SummaryTableCell align="center">{`$${income}`}</SummaryTableCell>
           </tr>
         ))}
       </SummaryTable>
